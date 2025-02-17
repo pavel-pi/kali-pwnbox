baae2c59297fed47f3dcd10b5cdb12a746b2adeb
pavel-pi <pavel.post@mailbox.org>
2025-02-17 13:50:46 +0100

    Update vpnbash.sh to contain 192.x.x.x addresses

--- vpnbash.sh
+++ vpnbash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-htbip=$(ip addr | grep -E "(tun0|tap0)" | grep inet | grep -E "(10.|172.)" | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+htbip=$(ip addr | grep -E "(tun0|tap0)" | grep inet | grep -E "(10.|172.|192.)" | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
 
-if [[ $htbip == *"10."* || $htbip == *"172."* ]]
+if [[ $htbip == *"10."* || $htbip == *"172."* || $htbip == *"192."* ]]
 then
    echo "$htbip"
 else
